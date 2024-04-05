ebd5d44b7a2cf32441391b5f95a9441cddb44d2b
AJ <ajblankenship1@gmail.com>
2024-04-05 00:19:22 +0000

    first commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,89 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import {useState} from 'react';
 
 export default function Home() {
+  const [initialNumber,setInitialNumber] = useState("");
+  const [exponentNumber,setExponentNumber] =useState("");
+  const [answerBox, setAnswerBox] = useState("Answer Will Appear Here");
+  const [initialNumberPlaceholder, setInitialNumberPlaceholder] = useState("this number");
+  const [exponentPlaceholder, setExponentPlaceholder] = useState("to the power of this number");
+  const [initialNumberClass, setInitialNumberClass] = useState("initialNumberOK");
+  const [exponentNumberClass,setExponentNumberClass] = useState("exponentNumberOK");
+  const [initialNumberLabel,setInitialNumberLabel] = useState("");
+  const [exponentNumberLabel, setExponentNumberLabel] = useState("");
+  const [initialNumberLabelClass, setInitialNumberLabelClass] = useState("");
+  const [exponentNumberLabelClass, setExponentNumberLabelClass] = useState("");
+
+  function handleInputNumber(event){
+    setInitialNumber(event.target.value);
+  
+  }
+
+  function handleInputExponent(event){
+    setExponentNumber(event.target.value)
+  }
+
+  function handleClickAnswer(){
+    let testInitial = Number(initialNumber);
+    let testExponent = Number(exponentNumber);
+    if(isNaN(testInitial) && isNaN(testExponent)){
+      setInitialNumberClass("initialNumberWrong");
+      setInitialNumberLabel("this must me a NUMBER");
+      setInitialNumberLabelClass("initialNumberWrongLabel");
+      setExponentNumberClass("exponentNumberWrong");
+      setExponentNumberLabel("this must me a NUMBER");
+      setExponentNumberLabelClass("exponentNumbeWrongrLabel");
+      setAnswerBox("???");
+      setInitialNumber("Click CLEAR and try again");
+      setExponentNumber("Click CLEAR and try again");
+    
+    }
+    else if(isNaN(testInitial)){
+    setInitialNumberClass("initialNumberWrong");
+    setInitialNumberLabel("this must me a NUMBER");
+    setInitialNumberLabelClass("initialNumberWrongLabel");
+    setAnswerBox("???");
+    setInitialNumber("Click CLEAR and try again");
+    }else if(isNaN(testExponent)){
+    setExponentNumberClass("exponentNumberWrong");
+    setExponentNumberLabel("this must me a NUMBER");
+    setExponentNumberLabelClass("exponentNumbeWrongrLabel");
+    setExponentNumber("Click CLEAR and try again");
+    setAnswerBox("???");
+    }else{
+    setAnswerBox(initialNumber**exponentNumber);
+    }
+   
+  }
+
+  function handleClickClearAll(){
+    setAnswerBox("Answer Will Appear Here");
+    setInitialNumber("");
+    setExponentNumber("");
+    setInitialNumberClass("initialNumberOK");
+    setExponentNumberClass("exponentNumberOK");
+    setExponentNumberLabel("");
+    setInitialNumberLabel("");
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main>
+      <div className="calcBox">
+        <h1>Exponent Calculator</h1>
+        <form className='calculator'>
+          <div className='initialNumberDiv'>
+            <label className={initialNumberLabelClass}>{initialNumberLabel}</label>
+            <input type="text" placeholder={initialNumberPlaceholder} className={initialNumberClass} value={initialNumber} onChange={handleInputNumber}/>
+          </div>
+          <p className="toThePowerOf"> ^ </p>
+          <div className='exponentNumberDiv'>
+            <label className={exponentNumberLabelClass}>{exponentNumberLabel}</label>
+            <input type="text" placeholder={exponentPlaceholder} className={exponentNumberClass} value={exponentNumber} onChange={handleInputExponent}/>
+          </div>
+        </form>
+        <button className="answerButton" onClick={handleClickAnswer}> = </button>
+        <div className="answerBox"> {answerBox} </div>
+        <button className="clearButton" onClick={handleClickClearAll}>CLEAR</button>
       </div>
     </main>
   );
